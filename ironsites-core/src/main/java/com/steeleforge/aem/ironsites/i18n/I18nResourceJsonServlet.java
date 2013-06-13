b38ae95aa9ffccbdf718aa6f172b1eb4fa95548c
David Steele <davidcsteele@gmail.com>
2013-06-12 22:34:51 -0500

    Enhancements to i18nHelper and movement of dox to wiki

---
+++ ironsites-core/src/main/java/com/steeleforge/aem/ironsites/i18n/I18nResourceJsonServlet.java
@@ -0,0 +1,76 @@
+package com.steeleforge.aem.ironsites.i18n;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.commons.osgi.PropertiesUtil;
+import org.osgi.service.component.ComponentContext;
+
+import com.day.cq.wcm.api.LanguageManager;
+import com.google.gson.Gson;
+
+@SlingServlet(label = "ironsites - I18n Resource JSON Servlet",
+	description = "JSON serialization of sling:MessageEntry based on sling:esourceType",
+	name = "com.steeleforge.aem.ironsites.i18n.I18nResourceJsonServlet",
+	resourceTypes = { "sling/servlet/default" },
+	metatype = true, extensions = { "json" }, selectors = { "i18n" })
+public class I18nResourceJsonServlet extends SlingSafeMethodsServlet {
+	
+	private static final long serialVersionUID = -8837830903603951318L;
+
+	Gson gson = null;
+	ComponentContext componentContext = null;
+	static final boolean DEFAULT_ENABLED = true;
+	
+	@Reference
+	LanguageManager languageManager;
+	
+    @Property(label = "Enable", 
+    		description = "Enable JSON serialization of sling:MessageEntry resolution by resource type",
+    		boolValue = DEFAULT_ENABLED)
+	static final String PROPERTY_ENABLED = "enabled";
+    protected Boolean enabled;
+	
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response)
+			throws ServletException, IOException {
+		Resource resource = request.getResource();
+		if (enabled && null != resource) {
+			ResourceBundle bundle = request.getResourceBundle(resource.getResourceType(), 
+				languageManager.getLanguage(resource));
+			I18nResourceBundle i18nBundle = new I18nResourceBundle(null, bundle);
+
+			// utilizing I18nResourceBundle wrapper for locale-specific bundle
+			// this is for adaptTo(ValueMap) for serialization.
+			// Gson#toJson against ResourceBundle causes stackoverflow.
+			String json = gson.toJson(i18nBundle.adaptTo(ValueMap.class));
+			
+			byte[] jsonBytes = json.getBytes("UTF-8");
+			
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.setContentLength(jsonBytes.length);
+			response.getOutputStream().write(jsonBytes);
+		} else {
+			response.sendError(SlingHttpServletResponse.SC_NOT_FOUND);
+		}
+	}
+	
+	@Activate
+	protected void activate(ComponentContext context) {
+		this.componentContext = context;
+		this.gson = new Gson();
+		this.enabled = PropertiesUtil.toBoolean(context.getProperties()
+				.get(PROPERTY_ENABLED), DEFAULT_ENABLED);
+	}
+}
---
+++ ironsites-core/src/main/java/com/steeleforge/aem/ironsites/i18n/I18nUtil.java
@@ -0,0 +1,127 @@
+package com.steeleforge.aem.ironsites.i18n;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+
+public enum I18nUtil {
+	INSTANCE;
+	private static final String TOKEN_RULE_DELIMITER = ";";
+	private static final String TOKEN_REPLACEMENT_DELIMITER = "=";
+	private static final String DEFAULT_FORMAT = "%s";
+	
+
+	/**
+	 * Interprets variable replacement as well as formatting; Object[] version
+	 * 
+	 * @param src source text
+	 * @param rules see {@link I18nUtil#interpolate(String, String)}
+	 * @param values
+	 * @return
+	 */
+	public static String interpolateValues(String src, String rules, Object[] values) {
+		return interpolate(swap(src, rules), values);
+	}
+	
+	/**
+	 * Interprets variable replacement as well as formatting; varargs version
+	 * 
+	 * @param src source text
+	 * @param rules see {@link I18nUtil#interpolate(String, String)}
+	 * @param values
+	 * @return
+	 */
+	public static String interpolate(String src, String rules, Object... values) {
+		return interpolate(swap(src, rules), values);
+	}
+	
+	/**
+	 * Supports MessageFormat varargs to replace source text tokens.
+	 * 
+	 * @param src source text
+	 * @param values replacement tokens
+	 * @return
+	 */
+	public static String interpolate(String src, Object... values) {
+		if (StringUtils.isBlank(src)) {
+			return src;
+		}
+		// to behave similar to I18n#get
+		return MessageFormat.format(StringUtils.replace(src, "'", "''"), values);
+	}
+	
+	/**
+	 * Replace named tokens with MessageFormat indices.
+	 * 
+	 * @param src source text
+	 * @param variables array of variables to replace with indicees
+	 * @param indices array of indices to replace variables
+	 * @return
+	 */
+	public static String interpolate(String src, String[] variables, String[] indices) {
+		if (StringUtils.isBlank(src)) {
+			return src;
+		}
+		return StringUtils.replaceEach(src, variables, indices);
+	}
+	
+	/**
+	 * Replace named tokens with MessageFormat indices.
+	 * 
+	 * Expects replacement rule definition to be formatted as "foo=0;bar=1" etc
+	 * whitespace is ignored; strict interpretation of "right replaces left"
+	 * 
+	 * @param src source text
+	 * @param tokens rule definition; format "foo=0;bar=1"; ignores whitespace
+	 * @param format token format
+	 * @return
+	 */
+	public static String swap(String src, String rules, String format) {
+		if (StringUtils.isBlank(src)) {
+			return src;
+		}
+		String fmt = (StringUtils.isNotBlank(format))? format : DEFAULT_FORMAT;
+		List<String> indices = Collections.emptyList();
+		List<String> variables = Collections.emptyList();
+		if (StringUtils.isNotBlank(rules)) {
+			// split by delimiter then populate indices & assignment lists
+			// variables[idx] should be a variable name, whitespace stripped
+			// indices[idx] should be an index such as 0, whitespace stripped
+			// each will be wrapped with { }'s for MessageFormat#format
+			String[] pairs;
+			String[] parts; 
+			pairs = StringUtils.split(rules, TOKEN_RULE_DELIMITER);
+			if (pairs.length > 0) {
+				indices = new ArrayList<String>();
+				variables = new ArrayList<String>();
+				for(String rule : pairs) {
+					parts = StringUtils.split(rule, TOKEN_REPLACEMENT_DELIMITER);
+					if (2 == ArrayUtils.getLength(parts)) {
+						variables.add(String.format(fmt, StringUtils.trim(parts[0])));
+						indices.add(String.format(fmt, StringUtils.trim(parts[1])));
+					}
+				}
+			}
+			pairs = null;
+			parts = null;
+		}
+		return interpolate(src, variables.toArray(new String[0]), indices.toArray(new String[0]));
+	}
+	
+
+	/**
+	 * Interpolate with default java index format "{%s}"
+	 * 
+	 * @param src
+	 * @param rules
+	 * @see I18nUtil#interpolate(String, String, String)
+	 * @return
+	 */
+	public static String swap(String src, String rules) {
+		return swap(src, rules, DEFAULT_FORMAT);
+	}
+}
