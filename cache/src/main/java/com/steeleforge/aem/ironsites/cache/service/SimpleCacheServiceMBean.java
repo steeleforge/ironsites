93f7f7d9393196e950285e3b45655e64cb59b73f
David Steele <davidcsteele@gmail.com>
2014-08-29 02:35:13 -0500

    SimpleCache MBeans! JMX & cURL to felix can invalidate SimpleCache instances. Fixed i18n node names.

---
+++ cache/src/main/java/com/steeleforge/aem/ironsites/cache/service/SimpleCacheServiceMBean.java
@@ -0,0 +1,47 @@
+/** 
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org/>
+ */
+package com.steeleforge.aem.ironsites.cache.service;
+
+import java.util.Set;
+
+import com.adobe.granite.jmx.annotation.Description;
+import com.google.common.cache.CacheStats;
+
+@Description("SimpleCache MBean")
+public interface SimpleCacheServiceMBean {
+    @Description("Caches")
+    public Set<String> getCacheNames();
+
+    @Description("Cache Size")
+    public long getCacheSize(String cache);
+    
+    @Description("Cache Access/Miss Statistics")
+    public CacheStats getCacheStats(String cache);
+
+    @Description("Invalidate Cache")
+    public void clearCache(String cache);
+}
---
+++ cache/src/main/java/com/steeleforge/aem/ironsites/cache/service/impl/SimpleCacheServiceMBeanImpl.java
@@ -0,0 +1,76 @@
+/** 
+ * This is free and unencumbered software released into the public domain.
+ * 
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ * 
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * For more information, please refer to <http://unlicense.org/>
+ */
+package com.steeleforge.aem.ironsites.cache.service.impl;
+
+import java.util.Set;
+
+import javax.management.DynamicMBean;
+import javax.management.NotCompliantMBeanException;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+
+import com.adobe.granite.jmx.annotation.AnnotatedStandardMBean;
+import com.google.common.cache.CacheStats;
+import com.steeleforge.aem.ironsites.cache.service.SimpleCacheService;
+import com.steeleforge.aem.ironsites.cache.service.SimpleCacheServiceMBean;
+
+@Service(value = DynamicMBean.class)
+@Component(name = "SimpleCacheService JMX", immediate = true)
+@Property(name = "jmx.objectname", value = "com.steeleforge.aem.ironsites.cache:type=SimpleCacheServiceMBean")
+public class SimpleCacheServiceMBeanImpl extends AnnotatedStandardMBean implements SimpleCacheServiceMBean {
+
+    @Reference
+    private SimpleCacheService cacheService;
+
+    public SimpleCacheServiceMBeanImpl() throws NotCompliantMBeanException {
+        super(SimpleCacheServiceMBean.class);
+    }
+
+    @Override
+    public Set<String> getCacheNames() {
+        return cacheService.getCaches();
+    }
+
+    @Override
+    public long getCacheSize(String cacheName) {
+        return cacheService.getCache(cacheName).size();
+    }
+
+    @Override
+    public CacheStats getCacheStats(String cacheName) {
+        return cacheService.getStats(cacheName);
+    }
+    
+    @Override
+    public void clearCache(String cacheName) {
+        cacheService.getCache(cacheName).invalidateAll();
+    }
+
+}
