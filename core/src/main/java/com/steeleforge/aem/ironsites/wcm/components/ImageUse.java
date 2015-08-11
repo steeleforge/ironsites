7900104f1ee38bd1eb59fdd6597ad78ddf9cda65
David Steele <davidcsteele@gmail.com>
2015-08-11 13:06:28 -0500

    updates to adaptive to suit a new responsive image component

--- core/src/main/java/com/steeleforge/aem/ironsites/wcm/components/ImageUse.java
+++ core/src/main/java/com/steeleforge/aem/ironsites/wcm/components/ImageUse.java
@@ -35,6 +35,7 @@ import java.util.Map;
 
 import javax.jcr.RepositoryException;
 
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.sling.api.resource.Resource;
 
 import com.adobe.cq.sightly.WCMUse;
@@ -43,6 +44,7 @@ import com.steeleforge.aem.ironsites.wcm.servlet.AdaptiveImageConfig;
 public class ImageUse extends WCMUse {
     // statics
     private static final String PN_PATH_IMAGE = "image";
+    private static final String PN_WIDTHS_IMAGE = "widths";
     private static final String DEFAULT_PATH_IMAGE = "/image";
     private static final String ATTR_SOURCE_SRCSET = "srcset";
     private static final String ATTR_SOURCE_MEDIA_MINWIDTH = "minWidth";
@@ -50,6 +52,7 @@ public class ImageUse extends WCMUse {
     private static final String FMT_SOURCE_FULL = "{0}.img.full.high.{1}{2}";
     
     // local
+    private List<Integer> widths = null;
     private IronImage image = null;
     private List<Map<String, String>> sources = null;
 
@@ -57,7 +60,8 @@ public class ImageUse extends WCMUse {
     public void activate() throws Exception {
         final AdaptiveImageConfig config = getSlingScriptHelper().getService(AdaptiveImageConfig.class);
         image = setImage();
-        sources = setSources(config.getWidths());
+        widths = setWidths(config.getWidths());
+        sources = setSources(widths);
     }
 
     public IronImage getImage() {
@@ -85,12 +89,39 @@ public class ImageUse extends WCMUse {
         Resource imageResource = null;
         String resourcePath = get(PN_PATH_IMAGE, String.class);
         if (null != resourcePath) {
-            imageResource = getResourceResolver().resolve(resourcePath);
+            imageResource = getResourceResolver().resolve(getResource().getPath() 
+                    + resourcePath);
         } else {
             imageResource = getResourceResolver().resolve(getResource().getPath() 
                     + DEFAULT_PATH_IMAGE);
         }
         return new IronImage(imageResource);
+    }
+    
+    /**
+     * initialize widths
+     * 
+     * @return
+     * @throws RepositoryException
+     */
+    private List<Integer> setWidths(List<Integer> configWidths) {
+        widths = new LinkedList<Integer>();
+        Object[] optionWidths = get(PN_WIDTHS_IMAGE, Object[].class);
+        if (ArrayUtils.getLength(optionWidths) > 0) {
+            // loop through Use width options for a given image
+            // allow only those matching configWidths
+            for(Object width : optionWidths) {
+                // allow only those matching configWidths
+                if (null != width && configWidths.contains(Integer.parseInt(width.toString()))) {
+                    widths.add(Integer.parseInt(width.toString()));
+                }
+            }
+        } else {
+            // no widths provided to ImageUse
+            widths.addAll(configWidths);
+        }
+        Collections.sort(widths);
+        return widths;
     }
     
     /**
@@ -105,21 +136,19 @@ public class ImageUse extends WCMUse {
             Integer prev = 0;
             for(Integer width : widths) {
                 // prevent srcset for widths exceeding original asset width
-                if (width <= getImage().getWidth()) {
-                    Map<String,String> attrs = new HashMap<String, String>();
-                    prev += 1;
-                    // update next min-width media query
-                    // source tag attributes
-                    attrs.put(ATTR_SOURCE_SRCSET, 
-                            MessageFormat.format(FMT_SOURCE_SRCSET,
-                                    getImage().getPath(), 
-                                    width, 
-                                    getImage().getExtension(), 
-                                    getImage().getSuffix()));
-                    attrs.put(ATTR_SOURCE_MEDIA_MINWIDTH, Integer.toString(prev));
-                    prev = width;
-                    sources.add(attrs);
-                }
+                Map<String,String> attrs = new HashMap<String, String>();
+                prev += 1;
+                // update next min-width media query
+                // source tag attributes
+                attrs.put(ATTR_SOURCE_SRCSET, 
+                        MessageFormat.format(FMT_SOURCE_SRCSET,
+                                getImage().getPath(), 
+                                width, 
+                                getImage().getExtension(), 
+                                getImage().getSuffix()));
+                attrs.put(ATTR_SOURCE_MEDIA_MINWIDTH, Integer.toString(prev));
+                prev = width;
+                sources.add(attrs);
             }
         }
         Collections.reverse(sources);
